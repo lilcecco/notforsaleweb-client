b46a5e035fb6447e51f1614ba76ff060950559c1
Matteo Ceccarelli <matteoceccarelli@Air-di-Matteo.homenet.telecomitalia.it>
2023-08-08 15:50:12 +0200

    stripe checkout updates

--- src/components/context/CheckoutContext.js
+++ src/components/context/CheckoutContext.js
@@ -13,11 +13,16 @@ export const CheckoutProvider = ({ children }) => {
 
   const [customerId, setCustomerId] = useState('');
 
-  let headers = new Headers();
-  useEffect(() => {
-    headers.append('Access-Control-Allow-Origin', '*');
-    headers.append('Access-Control-Allow-Credentials', 'true');
-  }, []);
+  // let headers = new Headers();
+  // useEffect(() => {
+  //   headers.append('Access-Control-Allow-Origin', '*');
+  //   headers.append('Access-Control-Allow-Credentials', 'true');
+  // }, []);
+
+  let headers = {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Credentials': 'true'
+  }
 
   useEffect(() => {
     const getCustomerId = async () => {
