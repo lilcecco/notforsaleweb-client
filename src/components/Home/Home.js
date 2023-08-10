33ef70d39189f5f39d6841abca101d570939bffa
Matteo Ceccarelli <matteoceccarelli@Air-di-Matteo-002.homenet.telecomitalia.it>
2023-08-10 03:38:15 +0200

    test product updates

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -19,7 +19,7 @@ const Home = () => {
       <div className="products-container" id="home-products">
         <h3>I Nostri Prodotti</h3>
         <div className="products mt-2">
-          {products.filter(product => product.name === 'test product').map(product => <Product key={`product-${product.id}`} product={product} />)}
+          {products.filter(product => product.name === 'test product' || product.name === 'UltraPods Gen 2 (seconda generazione)').map(product => <Product key={`product-${product.id}`} product={product} />)}
         </div>
       </div>
     </div>
